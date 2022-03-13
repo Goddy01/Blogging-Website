3312881fdc9f08e572f27eb9b54184c609443931
Godwin Adigun <adigungodwin2@gmail.com>
2022-03-13 21:16:29 +0100

    added the views for comment

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,17 +1,21 @@
 from django.urls import re_path
-from blog_app.views import AboutView, PostListView, PostDetailView, CreatePostView, UpdatePostView, DeletePostView, DeletePostView
-# , RegisterProfileView
+from blog_app.views import AboutView, PostListView, PostDetailView, CreatePostView, UpdatePostView, DeletePostView, DeletePostView, CreateProfileView, dele
+from blog_site.blog_app.views import DeleteProfileDeleteProfileView, DeleteProfileView, add_comment_to_post, comment_remove, publish_post
 
 app_name = 'blog_app'
 
 urlpatterns = [
-    re_path('^about/$', AboutView.as_view(), name='about'),
-    # re_path('^register/$', RegisterProfileView.as_view(), name='register'),
-    re_path('^$', PostListView.as_view(), name='post_list'),
-    re_path('^post/(?P<pk>\d+)/$', PostDetailView.as_view(), name='detail'),
-    re_path('^post/new_post/$', CreatePostView.as_view(), name='create_post'),
-    re_path('^post/update/(?P<pk>\d+)/edit/$', UpdatePostView.as_view(), name='update_post'),
-    re_path('^post/(?P<pk>\d+/)/delete\$', DeletePostView.as_view(), name='delete_post'),
-    re_path('^drafts/$', DeletePostView.as_view(), name='drafts')
-
+    re_path(r'^about/$', AboutView.as_view(), name='about'),
+    # re_path(r'^register/$', RegisterProfileView.as_view(), name='register'),
+    re_path(r'^$', PostListView.as_view(), name='post_list'),
+    re_path(r'^post/(?P<pk>\d+)/$', PostDetailView.as_view(), name='detail'),
+    re_path(r'^post/new_post/$', CreatePostView.as_view(), name='create_post'),
+    re_path(r'^post/update/(?P<pk>\d+)/edit/$', UpdatePostView.as_view(), name='update_post'),
+    re_path(r'^post/(?P<pk>\d+)/delete/$', DeletePostView.as_view(), name='delete_post'),
+    re_path(r'^drafts/$', DeletePostView.as_view(), name='drafts'),
+    re_path(r'^profile/create/$', CreateProfileView.as_view(), name='create_profile'),
+    re_path(r'^profile/(?P<pk>\d+)/delete/$', DeleteProfileView.as_view(), name='delete_profile'),
+    re_path(r'^post/(?P<pk>\d+)/comment/$', add_comment_to_post, name='add_comment_to_post'),
+    re_path(r'post/(?P<pk>\d+)/remove/$', comment_remove, name='remove_comment'),
+    re_path(r'post/(?P<pk>\d+)/publish/$', publish_post, name='publish_post')
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,5 +1,8 @@
+from tkinter import W
+from wave import Wave_write
+from django.contrib.auth.decorators import login_required
 from audioop import reverse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic import (TemplateView, DetailView, ListView, View, CreateView, UpdateView, DeleteView)
@@ -23,20 +26,22 @@ from blog_site.blog_app.models import CategoryTag, Profile
 #     template_name = 'index.html'
 
 class CreateProfileView(CreateView):
+    '''The view to create an account/profile.'''
     model = Profile
     form_class = ProfileForm
 
 class DeleteProfileView(LoginRequiredMixin, DeleteView):
+    '''The view to  delete an account/profile.'''
     login_url = '/login/'
     model = Profile
     form_class = ProfileForm
 
 class AboutView(TemplateView):
-    '''The view for the about page'''
+    '''The view for the about page.'''
     template_name = 'about.html'
 
 class PostListView(ListView):
-    '''The view for the series of posts made'''
+    '''The view for the series of posts made.'''
     model = Post
 
     # Formats how the list of posts should appear on the website in terms of date and order.
@@ -44,11 +49,11 @@ class PostListView(ListView):
         return Post.objects.filter(date_published__lte=timezone.now()).order_by('-date_published')
 
 class PostDetailView(DetailView):
-    '''The view that provides the details of a post'''
+    '''The view that provides the details of a post.'''
     model = Post
 
 class CreatePostView(LoginRequiredMixin, CreateView):
-    '''The view to create post'''
+    '''The view to create a post.'''
     login_url = '/login/' # The url the user is directed to in case they are not logged in.
     # fields  = ['author','title','image','text','date_created','date_published']
     # redirect_field_name 
@@ -57,7 +62,7 @@ class CreatePostView(LoginRequiredMixin, CreateView):
     model = Post
 
 class UpdatePostView(LoginRequiredMixin, UpdateView):
-    '''The view to update an existing post'''
+    '''The view to update an existing post.'''
     login_url = '/login/'
     # success_url = reverse_lazy('blog_app:detail')
     form_class = PostForm
@@ -71,6 +76,7 @@ class DeletePostView(LoginRequiredMixin, DeleteView):
     success_url = reverse_lazy('blog_app:post_list')
 
 class DraftListView(LoginRequiredMixin, ListView):
+    '''The view to get the list of drafts you have.'''
     model = Post
     login_url = '/login/'
     # redirect_field_name = 'blog_app:post_list'
@@ -80,3 +86,34 @@ class DraftListView(LoginRequiredMixin, ListView):
 
     # def get_success_url(self):
     #     return reverse_lazy('blog_app:post_list')
+
+###################
+#     COMMENT     #
+###################
+@login_required
+def publish_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('blog_app:post_list', pk=pk)
+
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment_form.save(commit=False)
+            comment_form.post = post
+            comment_form.save()
+            return redirect('blog_app:detail', pk=comment_form.post.pk)
+    else:
+        form = CommentForm()
+    return render(request, '../blog_app/templates/blog_app/comment_form.html', context={'form':form})
+
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('blog_app:post_detail', pk=post_pk)
